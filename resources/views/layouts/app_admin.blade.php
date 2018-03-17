230e046876b27ef03182011394e1f98c9f81c92b
Leo Nuneza <leiyu876@yahoo.com>
2018-03-17 14:31:59 +0300

    manage admin layouts

---
+++ resources/views/layouts/app_admin.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Blank Page</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="{{ asset('custom_adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('custom_adminlte/bower_components/font-awesome/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('custom_adminlte/bower_components/Ionicons/css/ionicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('custom_adminlte/dist/css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('custom_adminlte/dist/css/skins/_all-skins.min.css') }}">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+  @include('inc.admin.header')
+  @include('inc.admin.aside')
+  @yield('content')
+  @include('inc.admin.footer')
+</div>
+<script src="{{ asset('custom_adminlte/bower_components/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('custom_adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('custom_adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ asset('custom_adminlte/bower_components/fastclick/lib/fastclick.js') }}"></script>
+<script src="{{ asset('custom_adminlte/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('custom_adminlte/dist/js/demo.js') }}"></script>
+<script>
+  $(document).ready(function () {
+    $('.sidebar-menu').tree()
+  })
+</script>
+</body>
+</html>
