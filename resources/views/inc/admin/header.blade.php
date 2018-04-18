a4cfc2a5abece9c48aac2ec08bf672a05d1ecdc5
Leo D. Nuneza <leiyu876@yahoo.com>
2018-04-18 09:12:30 +0300

    dynamic name, remove some not relative text

--- resources/views/inc/admin/header.blade.php
+++ resources/views/inc/admin/header.blade.php
@@ -26,7 +26,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="{{ asset('custom_adminlte/dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs">{{ ucwords(Auth::user()->name) }}</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
@@ -34,30 +34,12 @@
                 <img src="{{ asset('custom_adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
+                  {{ ucwords(Auth::user()->name) }}
+                  <small>{{ Auth::user()->email }}</small>
                 </p>
-              </li>
-              <!-- Menu Body -->
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-                <!-- /.row -->
               </li>
               <!-- Menu Footer-->
               <li class="user-footer">
-                <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
-                </div>
                 <div class="pull-right">
                   <a href="{{ route('logout') }}" class="btn btn-default btn-flat" onclick="event.preventDefault();
                                    document.getElementById('logout-form').submit();">
