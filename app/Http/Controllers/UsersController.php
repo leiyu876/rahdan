894fcbefa896fc7ab9c0a6010167b72a875289c8
Leo D. Nuneza <leiyu876@yahoo.com>
2019-11-18 16:43:24 +0300

    In creating a user, limit password to 3 characters

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -58,7 +58,7 @@ class UsersController extends Controller
             'iqama' => 'required|unique:users',
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:3|confirmed',
             'roles' => 'required',
         ]);
 
