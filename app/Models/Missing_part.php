1c1bfb8f896ef49aa567b63affcea08104266a77
Leo Nuneza <leiyu876@yahoo.com>
2019-11-25 23:35:10 +0300

    In Missing Parts, use mutators in model for saving partno in Capital Letters

--- app/Models/Missing_part.php
+++ app/Models/Missing_part.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Missing_part extends Model
 {
     protected $guarded = [];
+
+    public function setPartnoAttribute($value)
+    {
+    	$this->attributes['partno'] = strtoupper($value);
+    }
 }
